76a93cc9ddc07509c8785f0473db8c23239fbea4
Vladislav Zenevsky <zenevsky.vladislav@yandex.ru>
2022-08-17 00:30:58 +0300

    test.py and README.md

--- tests.py
+++ tests.py
@@ -1,24 +1,69 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_init_default_books_rating_value_is_empty_dict(self):
         collector = BooksCollector()
+        assert collector.get_books_rating() == {}
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_add_book_default_rating_eq_1(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        assert collector.get_books_rating()['Алхимик'] == 1
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_add_the_same_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        collector.add_new_book('Алхимик')
+        assert len(collector.get_books_rating()) == 1
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_rating_set_valid_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        collector.set_book_rating('Алхимик', 5)
+        assert collector.get_books_rating()['Алхимик'] == 5
+
+    def test_set_book_rating_set_rating_more_than_10(self):
+        collector = BooksCollector()
+        collector.books_rating = {'Алхимик': 1}
+        collector.set_book_rating('Алхимик', 11)
+        assert collector.books_rating['Алхимик'] == 1
+
+    def test_get_book_rating_get_rating_for_book_that_not_in_list(self):
+        collector = BooksCollector()
+        assert collector.get_book_rating('Алхимик') is None
+
+    def test_get_books_with_specific_rating_get_books_with_rating_8(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        collector.set_book_rating('Алхимик', 7)
+        collector.add_new_book('Фауст')
+        collector.set_book_rating('Фауст', 8)
+        assert 'Фауст' in collector.get_books_with_specific_rating(8)
+
+    def test_get_books_rating_get_books_contained_in_books_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        collector.add_new_book('Фауст')
+        assert collector.get_books_rating() == {'Алхимик': 1, 'Фауст': 1}
+
+    def test_add_book_in_favorites_unable_to_add_book_that_not_in_books_rating(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Алхимик')
+        assert collector.get_list_of_favorites_books() == []
+
+    def test_delete_book_from_favorites_delete_book_contained_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        collector.add_book_in_favorites('Алхимик')
+        collector.delete_book_from_favorites('Алхимик')
+        assert 'Алхимик' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_get_book_contained_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Алхимик')
+        collector.add_new_book('Фауст')
+        collector.add_book_in_favorites('Алхимик')
+        collector.add_book_in_favorites('Фауст')
+        assert 'Алхимик' and 'Фауст' in collector.get_list_of_favorites_books()
